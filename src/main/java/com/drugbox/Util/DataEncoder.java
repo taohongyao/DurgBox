964d300128fb05597051192b8cf9608cd5ac3d2b
taohongyao <442472147@qq.com>
2016-03-13 11:56:56 +0800

    修复按药品名查询药品失败的BUG

--- src/main/java/com/drugbox/Util/DataEncoder.java
+++ src/main/java/com/drugbox/Util/DataEncoder.java
@@ -70,7 +70,7 @@ public class DataEncoder {
 
     public static String encodeStr(String str) {
         try {
-            return new String(str.getBytes("ISO-8859-1"), "UTF-8");
+            return new String(str.getBytes("UTF-8"), "UTF-8");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
             return null;
